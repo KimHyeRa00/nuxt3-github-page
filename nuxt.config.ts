43947266a87a88b9e62586b73d5e094b3b71596d
hrkim <hrkim@mannaplanet.co.kr>
2024-02-07 11:22:09 +0900

    초기파일

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  generate: {
+    // prerendering 설정
+    routes: [
+      // prerendering할 라우트 목록
+      '/',
+      '/page1',
+      '/page2',
+      // 기타 라우트...
+    ],
+  },
+  
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div>
+    메인
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
