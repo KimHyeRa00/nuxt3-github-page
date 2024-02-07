664472e7342232bed9de0900fd9c8d385f6c47f9
hrkim <hrkim@mannaplanet.co.kr>
2024-02-07 14:55:26 +0900

    base url 추가

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <div class="d-flex">
-     <nuxt-link to="/">main</nuxt-link> / 
+     <nuxt-link to="/">main2</nuxt-link> / 
      <nuxt-link to="/page1">page1</nuxt-link> /
      <nuxt-link to="/page2">page2</nuxt-link></div>
     메인
